7c9b57711d9126ca4f2cd6ab2af7c7ee1b28a6d3
Piero Toffanin <pt@masseranolabs.com>
2024-01-08 13:36:47 -0500

    More merge fixes

--- libs/MVS/Scene.cpp
+++ libs/MVS/Scene.cpp
@@ -1846,10 +1846,6 @@ size_t Scene::DrawCircle(PointCloud& pc, PointCloud::PointArr& outCircle, const 
 			pc.pointViews.emplace_back(views);
 			pc.normals.emplace_back(n);
 			pc.colors.emplace_back(Pixel8U::YELLOW);
-<<<<<<< HEAD
-			pc.pointWeights.emplace_back(PointCloud::WeightArr{1.f});
-=======
->>>>>>> cdc/master
 		}
 	}
 	return outCircle.size();
@@ -1928,11 +1924,6 @@ PointCloud Scene::BuildTowerMesh(const PointCloud& origPointCloud, const Point2f
 				const float aboveRad(circleRadii[ri - 1]);
 				float& circleRadius = circleRadii[ri];
 				const float belowRad(circleRadii[ri + 1]);
-<<<<<<< HEAD
-				const float AbvCrtDeltaPrc = ABS(aboveRad - circleRadius) / aboveRad;
-				const float BelCrtDeltaPrc = ABS(circleRadius - belowRad) / circleRadius;
-=======
->>>>>>> cdc/master
 				// set current radius as average of the most similar values in the closest 7 neighbors
 				if (ri > 2 && ri < circleRadii.size() - 5) {
 					FloatArr neighSeven(7);
@@ -2014,17 +2005,6 @@ PointCloud Scene::BuildTowerMesh(const PointCloud& origPointCloud, const Point2f
 							mesh.faces.emplace_back(v0, v2, v1);
 					}
 				}
-<<<<<<< HEAD
-				if (bInverted) {
-					topPoints.swap(botPoints);
-				}
-			}
-		}
-		mesh.Save("towermesh_dbg.ply");
-		towerPC.Save("cylinder.ply");
-	}
-	#endif
-=======
 				if (bInverted)
 					topPoints.swap(botPoints);
 			}
@@ -2036,7 +2016,6 @@ PointCloud Scene::BuildTowerMesh(const PointCloud& origPointCloud, const Point2f
 		mesh.Release();
 	}
 	towerPC.Save("tower.ply");
->>>>>>> cdc/master
 	return towerPC;
 }
 
@@ -2052,26 +2031,12 @@ void Scene::InitTowerScene(const int towerMode)
 	Point2f centerPoint;
 	if (!ComputeTowerCylinder(centerPoint, fRadius, fROIRadius, zMin, zMax, minCamZ, towerMode))
 		return;
-<<<<<<< HEAD
-	DEBUG("Scene camera positions identified ROI as a tower, select neighbors as if ROI is a tower");
-
-	// add nTargetPoints points on each circle
-	PointCloud towerPC(BuildTowerMesh(pointcloud, centerPoint, fRadius, fROIRadius, zMin, zMax, minCamZ, false));
-
-	switch (ABS(towerMode)) {
-	case 1: { // replace
-		pointcloud = std::move(towerPC);
-		break;
-	}
-	case 2: { // append
-=======
 
 	// add nTargetPoints points on each circle
 	PointCloud towerPC(BuildTowerMesh(pointcloud, centerPoint, fRadius, fROIRadius, zMin, zMax, minCamZ, false));
 	mesh.Release();
 
 	const auto AppendPointCloud = [this](const PointCloud& towerPC) {
->>>>>>> cdc/master
 		bool bHasNormal(pointcloud.normals.size() == pointcloud.GetSize());
 		bool bHasColor(pointcloud.colors.size() == pointcloud.GetSize());
 		bool bHasWeights(pointcloud.pointWeights.size() == pointcloud.GetSize());
@@ -2085,36 +2050,6 @@ void Scene::InitTowerScene(const int towerMode)
 			if (bHasWeights)
 				pointcloud.pointWeights.emplace_back(towerPC.pointWeights[idxPoint]);
 		}
-<<<<<<< HEAD
-		break;
-	}
-	case 3: { // select neighbors and remove added points
-		pointcloud.Swap(towerPC);
-		SelectNeighborViews(OPTDENSE::nMinViews, OPTDENSE::nMinViewsTrustPoint>1?OPTDENSE::nMinViewsTrustPoint:2, FD2R(OPTDENSE::fOptimAngle), OPTDENSE::nPointInsideROI);
-		pointcloud.Swap(towerPC);
-		break;
-	}
-	case 4: { // select neighbors
-		pointcloud.Swap(towerPC);
-		SelectNeighborViews(OPTDENSE::nMinViews, OPTDENSE::nMinViewsTrustPoint>1?OPTDENSE::nMinViewsTrustPoint:2, FD2R(OPTDENSE::fOptimAngle), OPTDENSE::nPointInsideROI);
-		pointcloud.Swap(towerPC);
-		bool bHasNormal(pointcloud.normals.size() == pointcloud.GetSize());
-		bool bHasColor(pointcloud.colors.size() == pointcloud.GetSize());
-		bool bHasWeights(pointcloud.pointWeights.size() == pointcloud.GetSize());
-		FOREACH(idxPoint, towerPC.points) {
-			pointcloud.points.emplace_back(towerPC.points[idxPoint]);
-			pointcloud.pointViews.emplace_back(towerPC.pointViews[idxPoint]);
-			if (bHasNormal)
-				pointcloud.normals.emplace_back(towerPC.normals[idxPoint]);
-			if (bHasColor)
-				pointcloud.colors.emplace_back(towerPC.colors[idxPoint]);
-			if (bHasWeights)
-				pointcloud.pointWeights.emplace_back(towerPC.pointWeights[idxPoint]);
-		}
-		break;
-	}
-	}
-=======
 	};
 
 	switch (ABS(towerMode)) {
@@ -2140,5 +2075,4 @@ void Scene::InitTowerScene(const int towerMode)
 		VERBOSE("Scene identified as tower-like; select view neighbors from detected tower point-cloud and next append it to existing point-cloud");
 		break;
 	}
->>>>>>> cdc/master
 } // InitTowerScene
